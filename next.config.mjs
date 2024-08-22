1e1ad6416c6d7760a0147328ea272ceeb051902b
kamar.ahajtan@octo.com <kamar.ahajtan@octo.com>
2024-08-22 17:54:25 +0200

    Replace local remotes URLs with deployed URLs

--- next.config.mjs
+++ next.config.mjs
@@ -9,8 +9,8 @@ export default {
                 filename: 'static/runtime/remoteEntry.js',
                 exposes: {},
                 remotes: {
-                    'acquisition': 'acquisition@http://localhost:3001/_next/static/runtime/remoteEntry.js',
-                    'suivi': 'suivi@http://localhost:3002/_next/static/runtime/remoteEntry.js'
+                    'acquisition': 'acquisition@https://gitlab.octo.tools:15095/micro-frontend-article-acquisition-tous-les-octo-f67fe2f398279a/_next/static/runtime/remoteEntry.js',
+                    'suivi': 'suivi@https://gitlab.octo.tools:15095/micro-frontend-article-suivi-tous-les-octos-atel-813b8855e51590/_next/static/runtime/remoteEntry.js'
                 }
             })
         );
