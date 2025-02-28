dde3375a3a06af2b7fa4c81bb1f7f5c144dd7753
Cédric Derue <cedric.derue@gmail.com>
2025-02-28 17:48:08 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,8 +9,8 @@ export default {
                 filename: 'static/runtime/remoteEntry.js',
                 exposes: {},
                 remotes: {
-                    'acquisition': 'acquisition@https://micro-frontends-acquisition-kamar3-1de56c66c399e13ffe08e5adad1c.gitlab.io/_next/static/runtime/remoteEntry.js',
-                    'suivi': 'suivi@https://micro-frontends-suivi-kamar3-24dfba73c19a6677503e30b0cc2692e5d3.gitlab.io/_next/static/runtime/remoteEntry.js'
+                    'acquisition': 'acquisition@https://raw.githubusercontent.com/cderue/acquisition/main/_next/static/runtime/remoteEntry.js',
+                    'suivi': 'suivi@https://raw.githubusercontent.com/cderue/suivi/main/_next/static/runtime/remoteEntry.js'
                 }
             })
         );
